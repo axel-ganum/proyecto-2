c70d52b0c052033fe8b4bde03829de3e59638774
NicolasGenta <nicolasgenta93@gmail.com>
2023-07-20 20:42:10 -0300

    segunda version

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import ProductList from "./componentes/productList"
-
+import React from 'react';
+import {BrowserRouter,Routes, Route, Link} from 'react-router-dom';
+import ProductList from "./componentes/productList";
+import Carrito from './componentes/carrito';
 
 
 function App() {
  
 
   return (
-    <>
-    <ProductList/>
+    <BrowserRouter>
+   <nav>
+    <Link to="/">Lista de Productos</Link>
+    <Link to="/about">Carrito</Link>
+    </nav>
+    
+    <div>
+      <Routes>
+      <Route path='/' element={<ProductList/>}/>
+      <Route path='/carrito' element={<Carrito/>}/>
+    </Routes>
 
+    </div>
+    </BrowserRouter>
+    
+    
 
-    </>
   )
 }
 
